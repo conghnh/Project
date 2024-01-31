b463d96afb42f0db4116094b2c939c4cd3bdc320
conghnh <hoangngochongcong94@gmail.com>
2024-01-31 10:25:33 +0700

    git add first

--- src/Bai1.java
+++ src/Bai1.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Bai1 {
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+
+    }
 }
